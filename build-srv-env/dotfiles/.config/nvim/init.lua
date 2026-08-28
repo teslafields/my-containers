a17684e0601ab96890fb8954bb50e2216a729e43
Benhur Tessele <benhur.tessele@est.tech>
2026-08-28 15:56:57 +0200

    added submodule

--- build-srv-env/dotfiles/.config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
--- Placeholder Neovim config seeded into the container on first run.
--- Replace this whole dotfiles/.config/nvim/ tree with your real config
--- before building the image (or drop it in later -- see README for how to
--- re-seed an existing volume).
-vim.opt.number = true
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
